e06da5d4b03dba1ac1fc5a1f7fce50aa1c4180f3
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2023-12-06 18:41:15 +0100

    merge auth corner e2e tests to one file

--- client/cypress/e2e/auth/auth-corner.cy.ts
+++ client/cypress/e2e/auth/auth-corner.cy.ts
@@ -1,10 +1,28 @@
-it('contain login button with routerLink', () => {
-  cy.createTestUser();
+describe('auth-corner should', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
-  cy.getByAttr('logout-button')
-    .should('be.visible')
-    .invoke('text')
-    .should('have.length.above', 1);
+  it('navigate to login on login button click', () => {
+    cy.getByAttr('login-button').click();
+    cy.url().should('not.include', 'register');
+    cy.url().should('include', 'login');
+  });
 
-  cy.deleteTestUser();
+  it('navigate to register on register button click', () => {
+    cy.getByAttr('register-button').click();
+    cy.url().should('not.include', 'login');
+    cy.url().should('include', 'register');
+  });
+
+  it('contain login button with routerLink', () => {
+    cy.createTestUser();
+
+    cy.getByAttr('logout-button')
+      .should('be.visible')
+      .invoke('text')
+      .should('have.length.above', 1);
+
+    cy.deleteTestUser();
+  });
 });
