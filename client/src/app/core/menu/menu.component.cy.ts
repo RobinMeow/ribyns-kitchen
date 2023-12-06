398d0cc6421c86c987d38d4992ce9140fa1a30a7
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2023-12-06 19:28:07 +0100

    fix menu component test

--- client/src/app/core/menu/menu.component.cy.ts
+++ client/src/app/core/menu/menu.component.cy.ts
@@ -1,8 +1,14 @@
+import { provideHttpClient } from '@angular/common/http';
+import { AuthDomainService } from '../auth/auth.domain.service';
 import { MenuComponent } from './menu.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('menu should', () => {
   beforeEach('mount', () => {
-    cy.mount(MenuComponent);
+    cy.mount(MenuComponent, {
+      imports: [RouterTestingModule],
+      providers: [provideHttpClient(), AuthDomainService],
+    });
   });
 
   it('be visible', () => {
