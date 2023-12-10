f4633ae10644443e31acc268d2c86d4aa407dd0b
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2023-12-10 13:31:48 +0100

    auth guards spect tests

--- client/src/app/core/auth/guards/authorized.guard.spec.ts
+++ client/src/app/core/auth/guards/authorized.guard.spec.ts
@@ -1,16 +1,30 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { authorizedGuard } from './authorized.guard';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('authorized guard should', () => {
   const executeGuard: CanActivateFn = (...guardParameters) =>
     TestBed.runInInjectionContext(() => authorizedGuard(...guardParameters));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
   });
 
   it('be created', () => {
     expect(executeGuard).toBeTruthy();
+  });
+
+  it('return false', () => {
+    expect(
+      executeGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot),
+    ).toBe(false);
   });
 });
