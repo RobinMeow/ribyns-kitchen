38c17976f23267767d92d17bf9eceef6e920a5f1
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2023-12-13 19:06:26 +0100

    JwtFactory tests

---
+++ server.tests/Infrastructure/JwtFactoryTests.cs
@@ -0,0 +1,50 @@
+﻿using api.Domain;
+using api.Domain.Auth;
+using api.Infrastructure.Auth;
+
+namespace server.tests.Infrastructure;
+
+public sealed class JwtFactoryTests
+{
+    [Theory]
+    [InlineData("0123456789012345678901234567890123456789012345678901234567890123")] // 512 bits (64 characters * 8)
+    [InlineData("0123456789012345678901234567890123456789012345678901234567890123456789")] // Larger than 512 bits
+    public void creates_token_with_key_which_as_at_least_512bits(string validKey)
+    {
+        // Arrange
+        var chef = new Chef("Chefname", EntityId.New());
+        var bearerConfig = new BearerConfig()
+        {
+            Audiences = ["Audience1"],
+            Issuer = "Issuer",
+            SigningKey = validKey
+        };
+
+        // Act
+        var jwtFactory = new JwtFactory(bearerConfig, new IssuerSigningKeyFactory(bearerConfig));
+        string jwt = jwtFactory.Create(chef);
+
+        // Assert
+        NotEmpty(jwt);
+    }
+
+    [Theory]
+    [InlineData("0123456789012345")] // 128 bits (16 characters * 8)
+    [InlineData("012345678901234567890123456789012345678901234567890123456789012")] // 504 bits (63 characters * 8)
+    public void creation_throws_with_key_which_as_less_than_512_bits(string invalidKey)
+    {
+        // Arrange
+        var chef = new Chef("Chefname", EntityId.New());
+        var bearerConfig = new BearerConfig()
+        {
+            Audiences = ["Audience1"],
+            Issuer = "Issuer",
+            SigningKey = invalidKey
+        };
+
+        var jwtFactory = new JwtFactory(bearerConfig, new IssuerSigningKeyFactory(bearerConfig));
+
+        // Act & Assert
+        ThrowsAny<Exception>(() => jwtFactory.Create(chef));
+    }
+}
--- server/Infrastructure/Auth/JwtFactory.cs
+++ server/Infrastructure/Auth/JwtFactory.cs
@@ -8,20 +8,26 @@ namespace api.Infrastructure.Auth;
 
 public sealed class JwtFactory : IJwtFactory
 {
-    BearerConfig _bearerConfig;
-    IIssuerSigningKeyFactory _issuerSigningKeyFactory;
+    readonly BearerConfig _bearerConfig;
+
+    readonly IIssuerSigningKeyFactory _issuerSigningKeyFactory;
 
     public JwtFactory(IConfiguration configuration, IIssuerSigningKeyFactory issuerSigningKeyFactory)
+        : this(configuration.GetSection(nameof(BearerConfig)).Get<BearerConfig>()
+            ?? throw new ArgumentException($"Missing dependency injection for {nameof(IConfiguration)}."), issuerSigningKeyFactory)
     {
+    }
+
+    public JwtFactory(BearerConfig bearerConfig, IIssuerSigningKeyFactory issuerSigningKeyFactory)
+    {
+        _bearerConfig = bearerConfig;
         _issuerSigningKeyFactory = issuerSigningKeyFactory;
-        _bearerConfig = configuration.GetSection(nameof(BearerConfig)).Get<BearerConfig>()
-            ?? throw new ArgumentException($"Missing dependency injection for {nameof(IConfiguration)}.");
     }
 
     public string Create(Chef chef)
     {
-        List<Claim> claims = new List<Claim>() {
-            new Claim(ClaimTypes.Name, chef.Name),
+        var claims = new List<Claim>() {
+            new (ClaimTypes.Name, chef.Name),
         };
 
         SecurityKey securityKey = _issuerSigningKeyFactory.Create();
@@ -33,7 +39,7 @@ public sealed class JwtFactory : IJwtFactory
             expires: DateTime.Now.AddDays(7),
             signingCredentials: signingCredentials,
             issuer: _bearerConfig.Issuer,
-            audience: String.Join(",", _bearerConfig.Audiences) // https://www.ibm.com/docs/en/datapower-gateway/2018.4?topic=commands-aud-claim "a comma-separated string of values"
+            audience: string.Join(",", _bearerConfig.Audiences) // https://www.ibm.com/docs/en/datapower-gateway/2018.4?topic=commands-aud-claim "a comma-separated string of values"
             );
 
         string jwt = new JwtSecurityTokenHandler().WriteToken(token);
