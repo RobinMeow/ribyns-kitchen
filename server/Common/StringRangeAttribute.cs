d77b36cb3500fa003ae6cd40d85106dad6f6d69c
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2023-12-13 18:05:46 +0100

    StringRangeAttribute tests

--- server/Common/StringRangeAttribute.cs
+++ server/Common/StringRangeAttribute.cs
@@ -5,6 +5,17 @@ namespace api;
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
 public sealed class StringRangeAttribute : StringLengthAttribute
 {
+    /// <summary>
+    /// define inclusive minimum and inclusive maximum character count.
+    /// <para>
+    /// <code>
+    /// [StringRange(0, 1)]
+    /// </code>
+    /// this one would allow <see cref="string.Empty" /> and "x", but not "xx"
+    /// </para>
+    /// </summary>
+    /// <param name="minimumLength">inclusive</param>
+    /// <param name="maximumLength">inclusive</param>
     public StringRangeAttribute(int minimumLength, int maximumLength) : base(maximumLength)
     {
         MinimumLength = minimumLength;
