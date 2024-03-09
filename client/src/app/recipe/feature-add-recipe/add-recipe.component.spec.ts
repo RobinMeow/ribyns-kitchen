0dd1d5f3f85048d3f1d4bd9f79f52c49bc7e89a0
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2024-03-09 10:54:27 +0100

    fix jest test

--- client/src/app/recipe/feature-add-recipe/add-recipe.component.spec.ts
+++ client/src/app/recipe/feature-add-recipe/add-recipe.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 import { AddRecipeComponent } from './add-recipe.component';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('AddRecipeComponent', () => {
   let component: AddRecipeComponent;
@@ -10,7 +12,11 @@ describe('AddRecipeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AddRecipeComponent],
-      providers: [provideNoopAnimations()],
+      providers: [
+        provideNoopAnimations(),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddRecipeComponent);
