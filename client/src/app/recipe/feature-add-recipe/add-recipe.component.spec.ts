4c97bebd4c8f80ad2533d13217969a9bd64cb7f4
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2024-03-09 11:22:24 +0100

    add jest and cy tests

--- client/src/app/recipe/feature-add-recipe/add-recipe.component.spec.ts
+++ client/src/app/recipe/feature-add-recipe/add-recipe.component.spec.ts
@@ -4,9 +4,11 @@ import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { AddRecipeComponent } from './add-recipe.component';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideHttpClient } from '@angular/common/http';
+import { RecipeService } from '@infrastructure/open-api';
 
-describe('AddRecipeComponent', () => {
+describe('AddRecipeComponent should', () => {
   let component: AddRecipeComponent;
+  let recipeService: RecipeService;
   let fixture: ComponentFixture<AddRecipeComponent>;
 
   beforeEach(async () => {
@@ -16,15 +18,31 @@ describe('AddRecipeComponent', () => {
         provideNoopAnimations(),
         provideHttpClient(),
         provideHttpClientTesting(),
+        RecipeService,
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddRecipeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    recipeService = TestBed.inject(RecipeService);
   });
 
-  it('should create', () => {
+  it('create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('not send http reuqest form is submitted invalid', async () => {
+    // form is invalid by default
+    const addAsyncSpy = jest.spyOn(recipeService, 'addAsync');
+
+    const submitButton = fixture.nativeElement.querySelector(
+      '[data-cy-add-recipe-submit-button]',
+    );
+
+    expect(submitButton.disabled).toBe(true);
+
+    submitButton.click();
+    expect(addAsyncSpy).not.toHaveBeenCalled();
   });
 });
