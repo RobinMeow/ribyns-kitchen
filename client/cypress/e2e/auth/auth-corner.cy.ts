31bea9da26ff296a6fcb657159b5f875981525f7
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2024-03-28 19:18:42 +0100

    update prettier which now supports angular control flow and save the cypress files for lint rules

--- client/cypress/e2e/auth/auth-corner.cy.ts
+++ client/cypress/e2e/auth/auth-corner.cy.ts
@@ -1,28 +1,28 @@
 describe('auth-corner should', () => {
   beforeEach(() => {
-    cy.visit('/');
-  });
+    cy.visit('/')
+  })
 
   it('navigate to login on login button click', () => {
-    cy.getByAttr('login-button').click();
-    cy.url().should('not.include', 'register');
-    cy.url().should('include', 'login');
-  });
+    cy.getByAttr('login-button').click()
+    cy.url().should('not.include', 'register')
+    cy.url().should('include', 'login')
+  })
 
   it('navigate to register on register button click', () => {
-    cy.getByAttr('register-button').click();
-    cy.url().should('not.include', 'login');
-    cy.url().should('include', 'register');
-  });
+    cy.getByAttr('register-button').click()
+    cy.url().should('not.include', 'login')
+    cy.url().should('include', 'register')
+  })
 
   it('contain logout button', () => {
-    cy.createTestUser();
+    cy.createTestUser()
 
     cy.getByAttr('logout-button')
       .should('be.visible')
       .invoke('text')
-      .should('have.length.above', 1);
+      .should('have.length.above', 1)
 
-    cy.deleteTestUser();
-  });
-});
+    cy.deleteTestUser()
+  })
+})
--- client/cypress/e2e/auth/auth.cy.ts
+++ client/cypress/e2e/auth/auth.cy.ts
@@ -1,54 +1,50 @@
 describe('auth should', () => {
   it('register > logout > login and delete', () => {
     // Register
-    cy.visit('/register');
+    cy.visit('/register')
     const credentials = {
       chefname: 'Cypress-Register',
-      password: 'iLoveJesus<3',
-    };
-    cy.getByAttr('register-submit-button').as('submit-btn');
+      password: 'iLoveJesus<3'
+    }
+    cy.getByAttr('register-submit-button').as('submit-btn')
 
-    cy.get('@submit-btn').should('be.disabled');
+    cy.get('@submit-btn').should('be.disabled')
 
-    cy.getByAttr('register-name-input').type(credentials.chefname);
-    cy.getByAttr('password-input').type(credentials.password);
+    cy.getByAttr('register-name-input').type(credentials.chefname)
+    cy.getByAttr('password-input').type(credentials.password)
 
-    cy.get('@submit-btn').should('be.enabled');
+    cy.get('@submit-btn').should('be.enabled')
     cy.intercept({
       path: '/Auth/RegisterAsync',
-      times: 1,
-    }).as('register-http-request');
-    cy.get('@submit-btn').click(); // should redirect somewhere in success
-    cy.wait('@register-http-request');
+      times: 1
+    }).as('register-http-request')
+    cy.get('@submit-btn').click() // should redirect somewhere in success
+    cy.wait('@register-http-request')
 
-    cy.url().should('not.include', 'register');
-    cy.getByAttr('auth-corner')
-      .getByAttr('logout-button')
-      .contains('Ausloggen');
+    cy.url().should('not.include', 'register')
+    cy.getByAttr('auth-corner').getByAttr('logout-button').contains('Ausloggen')
 
     // Logout
-    cy.getByAttr('auth-corner').getByAttr('logout-button').click();
+    cy.getByAttr('auth-corner').getByAttr('logout-button').click()
 
     // Login
-    cy.visit('/login');
-    cy.getByAttr('login-name-input').type(credentials.chefname);
-    cy.getByAttr('password-input').type(credentials.password);
-    cy.getByAttr('login-submit-button').click(); // should redirect somewhere in success
-    cy.url().should('not.include', 'login');
-    cy.getByAttr('auth-corner')
-      .getByAttr('logout-button')
-      .contains('Ausloggen');
+    cy.visit('/login')
+    cy.getByAttr('login-name-input').type(credentials.chefname)
+    cy.getByAttr('password-input').type(credentials.password)
+    cy.getByAttr('login-submit-button').click() // should redirect somewhere in success
+    cy.url().should('not.include', 'login')
+    cy.getByAttr('auth-corner').getByAttr('logout-button').contains('Ausloggen')
 
     // Delete the just registered account
-    cy.visit('/delete-chef');
-    cy.getByAttr('password-input').type(credentials.password);
+    cy.visit('/delete-chef')
+    cy.getByAttr('password-input').type(credentials.password)
     cy.intercept({
       path: '/Auth/DeleteAsync',
-      times: 1,
-    }).as('delete-http-request');
-    cy.getByAttr('delete-chef-form').submit(); // redirect on success
-    cy.wait('@delete-http-request');
-    cy.url().should('not.include', 'delete-chef');
-    cy.getByAttr('auth-corner').getByAttr('login-button');
-  });
-});
+      times: 1
+    }).as('delete-http-request')
+    cy.getByAttr('delete-chef-form').submit() // redirect on success
+    cy.wait('@delete-http-request')
+    cy.url().should('not.include', 'delete-chef')
+    cy.getByAttr('auth-corner').getByAttr('login-button')
+  })
+})
