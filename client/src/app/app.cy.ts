ac8ef1a13ff7ca881b914af3b4b8005e25caae81
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2024-03-28 20:41:34 +0100

    fix app test

--- client/src/app/app.cy.ts
+++ client/src/app/app.cy.ts
@@ -1,5 +1,5 @@
 it('render nothing apperently', () => {
   // no tests. Not sure if buggy, but App doesnt render anything.
-  cy.getByAttr('root').invoke('text').should('be.empty')
+  cy.get('[data-cy-root]').invoke('text').should('be.empty')
   // if this test fails, maybe there was an update, which allows App testing.
 })
