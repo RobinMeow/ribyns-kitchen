999f8af3f48344a46e4db4646daff9fe46d5c0e6
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2024-03-28 20:53:22 +0100

    you cant cypress test the app root I should write a wrapper

--- client/src/app/app.cy.ts
+++
@@ -1,5 +0,0 @@
-it('render nothing apperently', () => {
-  // no tests. Not sure if buggy, but App doesnt render anything.
-  cy.get('[data-cy-root]').invoke('text').should('be.empty')
-  // if this test fails, maybe there was an update, which allows App testing.
-})
