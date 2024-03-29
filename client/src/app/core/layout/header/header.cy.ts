87c1f26c9ee3fd913f86394dbbc8fca198a7ba7a
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2024-03-29 12:25:22 +0100

    another one

--- client/src/app/core/layout/header/header.cy.ts
+++ client/src/app/core/layout/header/header.cy.ts
@@ -1,11 +1,16 @@
 import { provideAppName } from 'src/app/core'
 import { Header } from './header'
 import { provideRouter } from '@angular/router'
+import { signal } from '@angular/core'
 
 describe('header should', () => {
   beforeEach('mount', () => {
     cy.mount(Header, {
-      providers: [provideAppName(), provideRouter([])]
+      providers: [provideAppName(), provideRouter([])],
+      componentProperties: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        hideMenuButton: signal(false) as any
+      }
     })
   })
 
