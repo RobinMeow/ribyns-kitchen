356a389794ab631bffa9c606b80d4a40dc9d04de
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2024-03-29 16:21:25 +0100

    remove register cy test

--- client/src/app/auth/feature-register/register.spec.ts
+++ client/src/app/auth/feature-register/register.spec.ts
@@ -5,6 +5,7 @@ import { provideNoopAnimations } from '@angular/platform-browser/animations'
 
 import { Register } from './register'
 import { provideApiBaseUrlTesting } from '@api'
+import { byTestAttr, setValue } from '@testing'
 
 describe('Register should', () => {
   let component: Register
@@ -28,5 +29,37 @@ describe('Register should', () => {
 
   it('create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('have title', () => {
+    expect(byTestAttr(fixture, 'title')).toBeTruthy()
+  })
+
+  it('have disabled submit button', () => {
+    const btn = byTestAttr<HTMLButtonElement>(fixture, 'register-submit-button')
+    expect(btn.disabled).toBeTrue()
+  })
+
+  it('have enabled submit button with valid form', () => {
+    const btn = byTestAttr<HTMLButtonElement>(fixture, 'register-submit-button')
+    expect(btn.disabled).toBeTrue()
+
+    const nameInput = byTestAttr<HTMLInputElement>(
+      fixture,
+      'register-name-input'
+    )
+    setValue(nameInput, 'Weinberg des Herrn')
+
+    fixture.detectChanges()
+    expect(btn.disabled).toBeTrue()
+
+    const passwordInput = byTestAttr<HTMLInputElement>(
+      fixture,
+      'password-input'
+    )
+    setValue(passwordInput, 'iLoveJesus<3')
+
+    fixture.detectChanges()
+    expect(btn.disabled).toBeFalse()
   })
 })
