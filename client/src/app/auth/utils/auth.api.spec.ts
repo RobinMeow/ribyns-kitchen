5cee3a89444ac92927f31d3a77a8628931b46138
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2024-03-30 13:52:01 +0100

    add lots of atuh tests

--- client/src/app/auth/utils/auth.api.spec.ts
+++ client/src/app/auth/utils/auth.api.spec.ts
@@ -5,27 +5,64 @@ import {
 import { TestBed } from '@angular/core/testing'
 import { AuthApi } from './auth.api'
 import { provideHttpClient } from '@angular/common/http'
-import { provideApiBaseUrlTesting } from '@api'
+import { API_BASE_URL, provideApiBaseUrlTesting } from '@api'
+import { Credentials } from '@auth'
+import { JwtToken } from '../JwtToken'
+import { ChefDto } from './ChefDto'
+import { RegisterChef } from './RegisterChef'
 
-describe('AuthApi should', () => {
-  let authApi: AuthApi
-  let httpTestingController: HttpTestingController
+/** to expose protected members */
+class AuthApiWrapper extends AuthApi {
+  constructor() {
+    super()
+  }
+  override deleteAsync(credentials: Credentials): Promise<void> {
+    return super.deleteAsync(credentials)
+  }
+  override loginAsync(credentials: Credentials): Promise<JwtToken> {
+    return super.loginAsync(credentials)
+  }
+  override registerAsync(newChef: RegisterChef): Promise<ChefDto> {
+    return super.registerAsync(newChef)
+  }
+}
+
+describe('AuthApi', () => {
+  let authApi: AuthApiWrapper
+  let httpCtrl: HttpTestingController
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        AuthApi,
+        { provide: AuthApiWrapper, useValue: new AuthApiWrapper() },
         provideHttpClient(),
         provideHttpClientTesting(),
-        provideApiBaseUrlTesting()
+        provideApiBaseUrlTesting(),
+        HttpTestingController
       ]
     })
-    authApi = TestBed.inject(AuthApi)
-    httpTestingController = TestBed.inject(HttpTestingController)
+    authApi = TestBed.inject(AuthApiWrapper)
+    httpCtrl = TestBed.inject(HttpTestingController)
   })
 
   afterEach(() => {
-    httpTestingController.verify()
+    httpCtrl.verify()
+  })
+
+  describe('deleteAsync should', () => {
+    it('make an http call', async () => {
+      console.log(authApi)
+      await expectAsync(
+        authApi.deleteAsync({
+          name: '',
+          password: ''
+        })
+      ).not.toBeResolved()
+
+      httpCtrl.expectOne((req) =>
+        req.url.includes(TestBed.inject(API_BASE_URL))
+      )
+    })
   })
 
   it('create', () => {
