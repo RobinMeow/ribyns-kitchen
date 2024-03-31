1a5371bb13e6ff6ac391819cc7564f4753b5b64e
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2024-03-31 22:07:58 +0200

    e2e for view recipe

---
+++ client/cypress/e2e/recipe/view-recipe.cy.ts
@@ -0,0 +1,29 @@
+describe('create-recipe should', () => {
+  it(`redirect to recipe/{recipeId} recipe is created sucessfully`, () => {
+    cy.task('db:reset')
+    cy.createTestUser()
+    // TODO this should use a seeded database
+
+    cy.visit('/create-recipe')
+
+    const recipeTitle = 'valid recipe title'
+
+    cy.getByAttr('recipe-title-input').type(recipeTitle)
+
+    cy.intercept({
+      path: '/Recipe/AddAsync',
+      times: 1
+    }).as('create-recipe')
+
+    cy.getByAttr('create-recipe-submit-button').click()
+
+    cy.wait('@create-recipe').then((stuff) => {
+      const newRecipe: { id: string } = stuff.response?.body
+      cy.url().should('include', 'recipe/' + newRecipe.id)
+    })
+
+    cy.getByAttr('title').contains(recipeTitle)
+
+    cy.deleteTestUser()
+  })
+})
