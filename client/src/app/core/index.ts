6f3e14779c1023349d4cb5aa23ff0e029fa478b3
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2024-04-01 19:02:11 +0200

    renames

--- client/src/app/core/index.ts
+++ client/src/app/core/index.ts
@@ -1,5 +1,5 @@
 export * from './core_routes'
-export * from './app-title-strategy/provideAppTitleStrategy'
+export * from './app-title-strategy/provide_app_title_strategy'
 export * from './layout/index'
 export * from './app-name/app_name'
-export * from './app-name/provideAppName'
+export * from './app-name/provide_app_name_temp'
