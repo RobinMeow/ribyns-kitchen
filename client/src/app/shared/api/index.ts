e7805f90f5f2189a1429ff4804d2e94761629af8
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2024-04-01 19:07:13 +0200

    provide api base url

--- client/src/app/shared/api/index.ts
+++ client/src/app/shared/api/index.ts
@@ -1,4 +1,4 @@
 export * from './api_base_url'
 export * from './base_api'
-export * from './provideApiBaseUrl'
-export * from './provideApiBaseUrlTesting'
+export * from './provide_api_base_url'
+export * from './provide_api_base_url_testing'
