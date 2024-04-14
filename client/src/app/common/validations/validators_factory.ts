a13f140fb7792fe2335d0f1193356900d1c40dee
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2024-04-14 12:29:37 +0200

    refac

--- client/src/app/common/validations/validators_factory.ts
+++ client/src/app/common/validations/validators_factory.ts
@@ -6,41 +6,36 @@ type CreateValidatorFn = (
   fieldConstraints: Readonly<FieldConstraints>
 ) => ValidatorFn[]
 
+function string_creator(fieldConstraints: Readonly<FieldConstraints>) {
+  const validators: ValidatorFn[] = []
+
+  const required = fieldConstraints.required
+  if (required) {
+    validators.push(Validators.required)
+  }
+
+  const min = fieldConstraints.min
+  if (min) {
+    validators.push(Validators.minLength(min))
+  }
+
+  const max = fieldConstraints.max
+  if (max) {
+    validators.push(Validators.maxLength(max))
+  }
+
+  return validators
+}
+
 export class ValidatorsFactory {
-  private readonly _creators: ReadonlyMap<string, CreateValidatorFn> = new Map<
-    string,
-    CreateValidatorFn
-  >([
-    [
-      'string',
-      (fieldConstraints: Readonly<FieldConstraints>) => {
-        const validators: ValidatorFn[] = []
-
-        const required = fieldConstraints.required
-        if (required) {
-          validators.push(Validators.required)
-        }
-
-        const min = fieldConstraints.min
-        if (min) {
-          validators.push(Validators.minLength(min))
-        }
-
-        const max = fieldConstraints.max
-        if (max) {
-          validators.push(Validators.maxLength(max))
-        }
-
-        return validators
-      }
-    ]
-  ])
+  private static readonly _creators: ReadonlyMap<string, CreateValidatorFn> =
+    new Map<string, CreateValidatorFn>([['string', string_creator]])
 
   create(
     dataType: 'string',
     fieldConstraints: FieldConstraints
   ): ValidatorFn[] {
-    const creator = this._creators.get(dataType)
+    const creator = ValidatorsFactory._creators.get(dataType)
     assert(creator, `No Validators Creator found for dataType: '${dataType}'`)
     return creator(fieldConstraints)
   }
