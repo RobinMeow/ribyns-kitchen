b9b6513742eb10dcab733fe86af9fb5a37217ff0
Robin Meow <robinmeow97@gmail.com>
2025-05-11 11:26:57 +0200

    refac

--- client/src/app/core/home-view/home.view.ts
+++ client/src/app/core/home-view/home.view.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 
 @Component({
-  standalone: true,
   templateUrl: './home.view.html',
   styleUrls: ['./home.view.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
