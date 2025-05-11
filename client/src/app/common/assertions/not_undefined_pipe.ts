80d72a0edcb77af4a80b8611c3e72cac882531e9
Robin Meow <robinmeow97@gmail.com>
2025-05-11 10:42:19 +0200

    refac

--- client/src/app/common/assertions/not_undefined_pipe.ts
+++ client/src/app/common/assertions/not_undefined_pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core'
 
-@Pipe({ name: 'notUndefined', pure: true })
+@Pipe({ name: 'notUndefined' })
 export class NotUndefinedPipe implements PipeTransform {
   transform<T>(obj: T | undefined): T {
     if (obj === undefined) throw new Error('obj may not be undefined.')
