10bfad3967d4ce0bb8b6d1171779c76fbb015b31
Robin Meow <robinmeow97@gmail.com>
2025-05-17 19:58:12 +0200

    add teardown with delete chef

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -95,11 +95,4 @@ export default defineConfig({
       dependencies: ['chef-setup']
     }
   ]
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 })
