5b5522555a24dc9defb2ca1fb5139f4f4abbad5b
Robin Meow <robinmeow97@gmail.com>
2025-05-17 16:08:11 +0200

    clean up scss styles and fix drawer menu for mobile

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,7 +1,9 @@
-import { Component, viewChild } from '@angular/core'
+import { Component, computed, inject, viewChild } from '@angular/core'
 import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav'
 import { RouterOutlet } from '@angular/router'
+import { BreakpointObserver } from '@angular/cdk/layout'
 import { Header, Menu } from 'src/app/core'
+import { toSignal } from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'rk-app',
@@ -11,6 +13,15 @@ import { Header, Menu } from 'src/app/core'
 })
 export class App {
   private readonly drawer = viewChild.required(MatDrawer)
+  private readonly breakpoints$ = inject(BreakpointObserver)
+
+  private readonly maxWidth600 = toSignal(
+    this.breakpoints$.observe('(max-width: 599.98px)')
+  )
+
+  protected readonly drawerMode = computed<'side' | 'over'>(() =>
+    this.maxWidth600()?.matches ? 'over' : 'side'
+  )
 
   protected onOpenMenuClick() {
     void this.drawer().open()
