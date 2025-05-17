5ba0b1ae3c35ed8731ceb9b3e183cb4ff043cf77
Robin Meow <robinmeow97@gmail.com>
2025-05-17 12:19:34 +0200

    fix e2e pipeline

--- client/cypress/e2e/recipe/view-recipe.spec.ts
+++ client/cypress/e2e/recipe/view-recipe.spec.ts
@@ -1,15 +1,15 @@
 describe('view-recipe should', () => {
   it(`redirect to recipe/{recipeId} recipe is created sucessfully`, async () => {
     const recipeId = crypto.randomUUID().toString()
-    const title = 'Cypress Recipe'
+    const name = 'Cypress Recipe'
     await (cy.task('db:reset') as unknown as Promise<null>)
     await (cy.task('db:seed:recipe', {
       id: recipeId,
-      title: title
+      name
     }) as unknown as Promise<null>)
     await cy.auth('register-and-login')
     cy.visit('/recipe/' + recipeId)
     cy.url().should('include', 'recipe/' + recipeId)
-    cy.byTestAttr('title').contains(title)
+    cy.byTestAttr('title').contains(name)
   })
 })
