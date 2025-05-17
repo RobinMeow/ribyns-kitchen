90f2bac2fb1158af6467defaa9eda4cf6270247f
Robin Meow <robinmeow97@gmail.com>
2025-05-17 18:38:33 +0200

    replace all data-test- attrs with the propper way of doing it

--- client/src/app/common/testing/by-test-attr.ts
+++ client/src/app/common/testing/by-test-attr.ts
@@ -6,12 +6,11 @@ import { ComponentFixture } from '@angular/core/testing'
  * with custom data-test attributes for testing purposes.
  *
  * @param fixture The ComponentFixture representing the component under test.
- * @param testid The value of the test attribute to search for, without the 'data-test-' prefix.
+ * @param testid The value assigned to the test attribute `data-test="the-value"`.
  * @returns The first matching element found in the component's template, or null if not found.
  *
  * @example
- * // Assumed html: <div data-test-example></div>
- * // alternatively: <div data-test="example"></div>
+ * // Assumed html: <div data-test="example"></div>
  * const div = queryByTestAttr<HTMLButtonElement>(fixture, 'example');
  * expect(div)toBeTruthy();
  */
@@ -20,16 +19,5 @@ export function byTestAttr<T>(
   testid: string
 ): T {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-  let element = fixture.nativeElement.querySelector(
-    `[data-test-${testid}]`
-  ) as T
-
-  if (!element) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-    element = fixture.nativeElement.querySelector(
-      `[data-test="${testid}"]`
-    ) as T
-  }
-
-  return element
+  return fixture.nativeElement.querySelector(`[data-test="${testid}"]`) as T
 }
