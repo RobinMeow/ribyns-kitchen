fb8ce3329a9f9af1460636af8520289b2c38e7da
Robin Meow <robinmeow97@gmail.com>
2025-05-18 20:50:42 +0200

    front end is no fun

--- client/src/app/recipe/new-recipe-view/new-recipe.view.ts
+++ client/src/app/recipe/new-recipe-view/new-recipe.view.ts
@@ -11,15 +11,18 @@ import { Router } from '@angular/router'
 import { NewRecipe } from '../util/new-recipe'
 import { Recipe } from '../util/recipe'
 import { RecipeApi } from '../util/recipe.api'
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   imports: [
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './new-recipe.view.html',
+  styleUrl: './new-recipe.view.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NewRecipeView {
@@ -37,7 +40,16 @@ export class NewRecipeView {
         Validators.minLength(this.name_min_length),
         Validators.maxLength(this.name_max_length)
       ]
-    ]
+    ],
+    variation: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(this.name_min_length),
+        Validators.maxLength(20)
+      ]
+    ],
+    portions: [2, [Validators.required, Validators.min(1)]],
   })
 
   protected async onSubmit(): Promise<void> {
